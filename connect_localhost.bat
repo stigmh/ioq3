b8d285b3a15a88f7b7989f05ae7a41457301885d
Stig M. Halvorsen <halsti@nith.no>
2014-02-11 13:01:53 +0100

    Added bat files for running MinGW compiles dedicated server and direct connect

---
+++ connect_localhost.bat
@@ -0,0 +1,1 @@
+./build/release-mingw32-x86/ioquake3.x86.exe +set r_fullscreen 0 +set fs_basepath "C:\Program Files\Steam\SteamApps\common\Quake 3 Arena" +connect 127.0.0.1
---
+++ launch_dedicated.bat
@@ -0,0 +1,1 @@
+./build/release-mingw32-x86/ioq3ded.x86.exe +set fs_basepath "C:\Program Files\Steam\SteamApps\common\Quake 3 Arena" +exec dedicated.cfg
