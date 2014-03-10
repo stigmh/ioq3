c96129e1e0f6e9be72cfb10914d4e06bd80cf255
Stig M. Halvorsen <halsti@nith.no>
2014-03-10 12:04:37 +0100

    Updated batch scripts for dedicated server creation and client connection

--- connect_localhost.bat
+++ connect_localhost.bat
@@ -1,1 +1,2 @@
-./build/release-mingw32-x86/ioquake3.x86.exe +set r_fullscreen 0 +set fs_basepath "C:\Program Files\Steam\SteamApps\common\Quake 3 Arena" +connect 127.0.0.1
+@echo off
+.\build\quake3_debug\ioquake3.x86.exe +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath "C:\Program Files (x86)\Steam\SteamApps\common\Quake 3 Arena" +connect 127.0.0.1
--- launch_dedicated.bat
+++ launch_dedicated.bat
@@ -1,1 +1,4 @@
-./build/release-mingw32-x86/ioq3ded.x86.exe +set fs_basepath "C:\Program Files\Steam\SteamApps\common\Quake 3 Arena" +exec dedicated.cfg
+@echo off
+set basepath="C:\Program Files (x86)\Steam\SteamApps\common\Quake 3 Arena"
+copy /Y dedicated_server.cfg %basepath%\baseq3
+.\build\ioquake3_dedicated_debug\ioq3ded.x86.exe +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath %basepath% +exec dedicated_server.cfg
