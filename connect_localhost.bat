74e4df1c8ef9afd2f864d65e85d8b00ecddb99fe
Stig M. Halvorsen <halsti@nith.no>
2014-03-17 11:41:36 +0100

    Added local file for bat scripts to make it easier to run them on different machines

--- connect_localhost.bat
+++ connect_localhost.bat
@@ -1,2 +1,4 @@
 @echo off
-.\build\quake3_debug\ioquake3.x86.exe +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath "C:\Program Files (x86)\Steam\SteamApps\common\Quake 3 Arena" +connect 127.0.0.1
+:: set basepath="C:\Program Files (x86)\Steam\SteamApps\common\Quake 3 Arena"
+call baseq3path.local.bat
+.\build\quake3_debug\ioquake3.x86.exe +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath %basepath% +connect 127.0.0.1
--- launch_dedicated.bat
+++ launch_dedicated.bat
@@ -1,4 +1,5 @@
 @echo off
-set basepath="C:\Program Files (x86)\Steam\SteamApps\common\Quake 3 Arena"
+:: set basepath="C:\Program Files (x86)\Steam\SteamApps\common\Quake 3 Arena"
+call baseq3path.local.bat
 copy /Y dedicated_server.cfg %basepath%\baseq3
 .\build\ioquake3_dedicated_debug\ioq3ded.x86.exe +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath %basepath% +exec dedicated_server.cfg
