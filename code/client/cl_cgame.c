91d87e8d515d247cabc33fb96dc7f9a0c4275dcd
Stig M. Halvorsen <halsti@nith.no>
2014-03-19 14:49:29 +0100

    Disabled pause if virtual client

--- code/client/cl_cgame.c
+++ code/client/cl_cgame.c
@@ -979,7 +979,7 @@ void CL_SetCGameTime( void ) {
 	}
 
 	// allow pause in single player
-	if ( sv_paused->integer && CL_CheckPaused() && com_sv_running->integer ) {
+	if ( sv_paused->integer && CL_CheckPaused() && com_sv_running->integer && !com_virtualClient->integer ) {
 		// paused
 		return;
 	}
