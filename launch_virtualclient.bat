95b82544ae26a8bc3ebb569ce305d1558f504b1c
Stig M. Halvorsen <halsti@nith.no>
2014-03-28 17:03:40 +0100

    Added bat script for launching of a virtual client, also creates log file.

---
+++ launch_virtualclient.bat
@@ -0,0 +1,4 @@
+@echo off
+:: set basepath="C:\Program Files (x86)\Steam\SteamApps\common\Quake 3 Arena"
+call baseq3path.local.bat
+.\build\quake3_debug\ioquake3.x86.exe +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath %basepath% +set virtualClient 1 +connect 127.0.0.1 2> 01_lcl.out.txt
