1650dc81d46e39d93e380fcf14513d4e0225af1c
Stig M. Halvorsen <halsti@nith.no>
2014-04-09 17:02:28 +0200

    Added parameter for server in Multiple VC launch bat script

--- launch_multiple_vcs.bat
+++ launch_multiple_vcs.bat
@@ -3,9 +3,14 @@ setlocal
 call baseq3path.local.bat
 
 set /a numClients=%1%
+set server=%2%
 
 if "%numClients%" == "" (
   set /a numClients=2
+)
+
+if "%server%" == "" (
+  set server=127.0.0.1
 )
 
 set clientNamePrefix=VC_
@@ -20,7 +25,7 @@ set /a skill=(5*%random%)/32768+1
 set /a randBot=(numBots*%random%)/32768+1
 for /f "tokens=%randBot%" %%f in ("%bots%") do set name=%%f 
 
-START /B .\build\quake3_debug\ioquake3.x86.exe +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath %basepath% +set virtualClient 2 +set virtualClientSkill %skill% +set virtualClientBot %name% +set virtualClientName %clientNamePrefix%%loopCounter% +connect 127.0.0.1 >nul 2>nul
+START /B .\build\quake3_debug\ioquake3.x86.exe +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath %basepath% +set virtualClient 2 +set virtualClientSkill %skill% +set virtualClientBot %name% +set virtualClientName %clientNamePrefix%%loopCounter% +connect %server% >nul 2>nul
 
 set /a loopCounter+=1
 set /a numClients-=1
