3524b879244bf85229f6e289530aebbce4e45dda
Stig M. Halvorsen <halsti@nith.no>
2014-04-09 10:04:22 +0200

    Made the multiple bot launch bat script work and customized the player in the other scripts

--- connect_localhost.bat
+++ connect_localhost.bat
@@ -1,4 +1,8 @@
 @echo off
 :: set basepath="C:\Program Files (x86)\Steam\SteamApps\common\Quake 3 Arena"
 call baseq3path.local.bat
-.\build\quake3_debug\ioquake3.x86.exe +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath %basepath% +connect 127.0.0.1
+
+set uname=Stigmha
+set model="sarge/krusade"
+
+.\build\quake3_debug\ioquake3.x86.exe +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath %basepath% +set team_headmodel %model% +set team_model %model% +set headmodel %model% +set model %model% +set name %uname% +connect 127.0.0.1
--- launch_virtualclient.bat
+++ launch_virtualclient.bat
@@ -1,4 +1,9 @@
 @echo off
 :: set basepath="C:\Program Files (x86)\Steam\SteamApps\common\Quake 3 Arena"
 call baseq3path.local.bat
-.\build\quake3_debug\ioquake3.x86.exe +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath %basepath% +set virtualClient 1 +set virtualClientSkill 2 +set virtualClientBot slash +set virtualClientName Ghris +connect 127.0.0.1 2> 01_lcl.out.txt
+
+set /a skill=5
+set botname=sarge
+set uname=Stigmha
+
+.\build\quake3_debug\ioquake3.x86.exe +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath %basepath% +set virtualClient 1 +set virtualClientSkill %skill% +set virtualClientBot %botname% +set virtualClientName %uname% +connect 127.0.0.1 2> 01_lcl.out.txt
