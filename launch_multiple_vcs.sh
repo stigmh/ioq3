12bcef1e5632d2c5402c2731a7af78ec83bb2444
Stig M. Halvorsen <halsti@nith.no>
2014-04-29 15:31:15 +0200

    Improved the multiplve VCs launch script and redirected output to null

--- launch_multiple_vcs.sh
+++ launch_multiple_vcs.sh
@@ -13,11 +13,11 @@ source baseq3path.local.sh
 numClients=$1
 server=$2
 
-if [[ "$numClients" = "" ]]; then
+if [[ -z $numClients ]]; then
   numClients=8
 fi
 
-if [[ "$server" = "" ]]; then
+if [[ -z $server ]]; then
   server="127.0.0.1"
 fi
 
@@ -33,10 +33,10 @@ loopCounter=0
 while [ $numClients -gt 0 ]; do
   let skill=($RANDOM%5)+1
   randBot=${bots[$RANDOM % $numBots]}
-  echo $randBot $skill $clientNamePrefix$loopCounter
+#  echo $randBot $skill $clientNamePrefix$loopCounter
   let numClients=numClients-1
   let loopCounter+=1
   
-  ./$buildDir/ioquake3.$arch +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath $basepath +set virtualClient 2 +set virtualClientSkill $skill +set virtualClientBot $randBot +set virtualClientName $clientNamePrefix$loopCounter +connect $server &
+  ./$buildDir/ioquake3.$arch +set r_fullscreen 0 +set sv_pure 0 +set vm_ui 0 +set vm_game 0 +set vm_cgame 0 +set fs_basepath $basepath +set virtualClient 2 +set virtualClientSkill $skill +set virtualClientBot $randBot +set virtualClientName $clientNamePrefix$loopCounter +connect $server >/dev/null 2>&1 &
 done
 
